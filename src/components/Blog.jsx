7e439a3cdb21f7f1bac7c34af55aa732355e2283
Maral Jargalsaikhan <maralisgoood@Marals-MacBook-Air.local>
2023-01-05 15:45:35 +0800

    blog added

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,9 +1,26 @@
 import "../styles/blog.css";
+import Twitter from "./icons/Twitter";
+import Linkin from "./icons/Linkin";
+import Medium from "./icons/Medium";
 
 export default function Blog() {
     return (
-        <div className="container">
-
+        <div id="blog" className="container">
+            <div className="blogText">
+                <h1>Blog Posts</h1>
+                <h2>I think so, this is it.</h2>
+                <p>Design begins after I begin to think about how to present an experience most successfully, whether a button I put in can solve a problem. The only point in design is not ui design, if the user does not have a good experience at the end of the product, the design will be considered unsuccessful in my opinion.</p>
+                <div className="linkBtns">
+                    <button className="twitterBtn"><Twitter/>twitter</button>
+                    <button className="linkedinBtn"><Linkin/>linkedin</button>
+                    <button className="mediumBtn"><Medium/>medium</button>
+                </div>
+            </div>
+            <div className="blogIll">
+                <img src="./images/Rectangle.png" alt="restangle" className="rectangle"></img>
+            <img src="./images/Saly-16.png" alt="Saly-16" class="saly"></img>
+            </div>
+            
         </div>
     );
 }
