852f65e5aa65839c5605fe1844400bce534e8fbb
Morning <morning@admins-iMac.local>
2023-01-05 13:38:02 +0800

    push

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { Data } from "./util/Data";
+import './App.css';
+import Header from "./components/Header";
+import Blog from "./components/Blog";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Blog />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,25 @@
+import "../styles/header.css";
+import Logo from "./icons/Logo";
+import Linkedin from "./icons/Linkedin";
+
+export default function Header() {
+    return (
+        <header className="container">
+            <div id="logo">
+                <Logo />
+                <button className="arrowBtn"><span><Linkedin /></span> Hire Me</button>
+            </div>
+            <nav>
+                <ul>
+                    <li>portfolio</li>
+                    <li className="active">blog</li>
+                    <li>cv</li>
+                    <li>store</li>
+                    <li>freelance</li>
+                    <li>about me</li>
+                    <li>contact</li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
